1be6e97c283414600ff532c0d67a7896ba76691c
mantekarys <46086217+mantekarys@users.noreply.github.com>
2022-04-01 08:25:12 +0300

    duombaze veikia

---
+++ WebAPI/WebAPI/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Project
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime date { get; set; }
+        public string description { get; set; }
+    }
+}
