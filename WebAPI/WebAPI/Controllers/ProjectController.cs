d9c2688cdc1c0e6ebb0c8827f6312ac5e69b9917
RokasBarkevicius <rokasbarkevicius@gmail.com>
2022-04-06 17:42:20 +0300

    done create and edit

--- WebAPI/WebAPI/Controllers/ProjectController.cs
+++ WebAPI/WebAPI/Controllers/ProjectController.cs
@@ -45,6 +45,153 @@ namespace WebAPI.Controllers
 
             return new JsonResult(table);
         }
+
+        [HttpPost]
+        public JsonResult insert(Project project)
+        {
+            string query0 = @"
+                    select * from dbo.Project where name='" + project.name + "'";
+                    
+            DataTable table0 = new DataTable();
+            string sqlDataSource0 = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader0;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource0))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query0, myCon))
+                {
+                    myReader0 = myCommand.ExecuteReader();
+                    table0.Load(myReader0); ;
+
+                    myReader0.Close();
+                    myCon.Close();
+                }
+            }
+            if (table0.Rows.Count > 0)
+            {
+                return new JsonResult("Project with this name already exists");
+            }
+            else
+            {
+
+                string query = @"
+                    insert into dbo.Project (name, date, description) values 
+                    ('" + project.name + "', CURRENT_TIMESTAMP, '" + project.description + @"')
+                    ";
+                DataTable table = new DataTable();
+                string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+                SqlDataReader myReader;
+                using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+                {
+                    myCon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader); ;
+
+                        myReader.Close();
+                        myCon.Close();
+                    }
+                }
+
+
+                string query1 = @"
+                    insert into dbo.Branch (foreign_project) 
+                    select top(1) id from dbo.Project order by  id desc
+                    ";
+                DataTable table1 = new DataTable();
+                string sqlDataSource1 = _configuration.GetConnectionString("EmployeeAppCon");
+                SqlDataReader myReader1;
+                using (SqlConnection myCon = new SqlConnection(sqlDataSource1))
+                {
+                    myCon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query1, myCon))
+                    {
+                        myReader1 = myCommand.ExecuteReader();
+                        table1.Load(myReader); ;
+
+                        myReader1.Close();
+                        myCon.Close();
+                    }
+                }
+
+                string query2 = @"
+                    insert into dbo.Version (foreign_branch, date) 
+                    select top(1) id , CURRENT_TIMESTAMP from dbo.Branch order by  id desc
+                    ";
+                DataTable table2 = new DataTable();
+                string sqlDataSource2 = _configuration.GetConnectionString("EmployeeAppCon");
+                SqlDataReader myReader2;
+                using (SqlConnection myCon = new SqlConnection(sqlDataSource2))
+                {
+                    myCon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query2, myCon))
+                    {
+                        myReader2 = myCommand.ExecuteReader();
+                        table2.Load(myReader); ;
+
+                        myReader2.Close();
+                        myCon.Close();
+                    }
+                }
+                return new JsonResult("Added Successfully");
+            }
+        }
+
+
+        [HttpPut]
+        public JsonResult Put(Project project)
+        {
+            string query0 = @"
+                    select * from dbo.Project where name='" + project.name + "'";
+
+            DataTable table0 = new DataTable();
+            string sqlDataSource0 = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader0;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource0))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query0, myCon))
+                {
+                    myReader0 = myCommand.ExecuteReader();
+                    table0.Load(myReader0); ;
+
+                    myReader0.Close();
+                    myCon.Close();
+                }
+            }
+            if (table0.Rows.Count > 0)
+            {
+                return new JsonResult("Project with this name already exists");
+            }
+            else
+            {
+                string query = @"
+                    update dbo.Project set 
+                    name = '" + project.name + @"',
+                    description = '" + project.description + @"'
+                    where id = " + project.id + @" 
+                    ";
+                DataTable table = new DataTable();
+                string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+                SqlDataReader myReader;
+                using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+                {
+                    myCon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader); ;
+
+                        myReader.Close();
+                        myCon.Close();
+                    }
+                }
+
+                return new JsonResult("Updated Successfully");
+            }
+        }
+
         //class ProjectE
         //{
         //    public int id;
@@ -141,7 +288,7 @@ namespace WebAPI.Controllers
         //                    }
         //                }
         //            }
-                    
+
 
         //            myCon.Close();
         //        }
--- my-app - Copy/src/CreateProject.js
+++ my-app - Copy/src/CreateProject.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React,{Component, useState} from 'react';
 import {Modal,Button, Row, Col, Form} from 'react-bootstrap';
 
 export class CreateProject extends Component{
@@ -7,17 +7,19 @@ export class CreateProject extends Component{
         this.handleSubmit=this.handleSubmit.bind(this);
     }
 
+
     handleSubmit(event){
         event.preventDefault();
-        fetch(process.env.REACT_APP_API+'department',{
+        fetch(process.env.REACT_APP_API+'project',{
             method:'POST',
             headers:{
                 'Accept':'application/json',
                 'Content-Type':'application/json'
             },
             body:JSON.stringify({
-                DepartmentId:null,
-                DepartmentName:event.target.DepartmentName.value
+                id:0,
+                name:event.target.name.value,
+                description:event.target.description.value
             })
         })
         .then(res=>res.json())
@@ -28,50 +30,54 @@ export class CreateProject extends Component{
             alert('Failed');
         })
     }
-    render(){
+
+    render(){     
         return (
             <div className="container">
+            <Modal
+            {...this.props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            >
+                <Modal.Header clooseButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Add Project
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
 
-<Modal
-{...this.props}
-size="lg"
-aria-labelledby="contained-modal-title-vcenter"
-centered
->
-    <Modal.Header clooseButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-            Add Department
-        </Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
+                    <Row>
+                        <Col sm={6}>
+                            <Form onSubmit={this.handleSubmit}>
+                                <Form.Group controlId="name">
+                                    <Form.Label>Project Name</Form.Label>
+                                    <Form.Control type="text" name="name" required 
+                                    placeholder=''/>
+                                </Form.Group>
+                                <Form.Group controlId="description">
+                                    <Form.Label>Project Description</Form.Label>
+                                    <Form.Control type="text" name="description" required 
+                                    placeholder=''/>
+                                </Form.Group>
 
-        <Row>
-            <Col sm={6}>
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group controlId="DepartmentName">
-                        <Form.Label>DepartmentName</Form.Label>
-                        <Form.Control type="text" name="DepartmentName" required 
-                        placeholder="DepartmentName"/>
-                    </Form.Group>
+                                <Form.Group>
+                                    <Button variant="primary" type="submit">
+                                        Add Project
+                                    </Button>
+                                </Form.Group>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Modal.Body>
+                
+                <Modal.Footer>
+                    <Button variant="danger" onClick={this.props.onHide}>Close</Button>
+                </Modal.Footer>
 
-                    <Form.Group>
-                        <Button variant="primary" type="submit">
-                            Add Department
-                        </Button>
-                    </Form.Group>
-                </Form>
-            </Col>
-        </Row>
-    </Modal.Body>
-    
-    <Modal.Footer>
-        <Button variant="danger" onClick={this.props.onHide}>Close</Button>
-    </Modal.Footer>
-
-</Modal>
+            </Modal>
 
             </div>
         )
     }
-
 }
